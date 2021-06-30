d33bb55b304f3d93dc393beb6fe1dcf9f55a0c0f
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-06-30 04:51:29 -0400

    Less mem

--- command_lines.sh
+++ command_lines.sh
@@ -1,9 +1,9 @@
 # Interactive job
-srun --partition interactive --job-name "InteractiveJob" --cpus-per-task 1 --mem-per-cpu 16G --time 2:00:00 --pty bash
+srun --partition interactive --job-name "InteractiveJob" --cpus-per-task 1 --mem-per-cpu 2G --time 2:00:00 --pty bash
 module load gcc/6.2.0 python/3.7.4
 
 # Interactive job + jupyter
-srun --partition interactive --job-name "InteractiveJob" --cpus-per-task 1 --mem-per-cpu 16G --time 4:00:00 --pty --x11 --tunnel 8888:8888 bash
+srun --partition interactive --job-name "InteractiveJob" --cpus-per-task 1 --mem-per-cpu 2G --time 4:00:00 --pty --x11 --tunnel 8888:8888 bash
 module load gcc/6.2.0 python/3.7.4
 source env_o2/bin/activate
 jupyter notebook casting/casting.ipynb --port=8888 --browser='none'
