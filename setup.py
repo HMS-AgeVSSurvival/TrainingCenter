e4ea6ace6cb9fc0ba4d5e849ae630c911dc329a6
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-08-22 05:01:52 -0400

    Test dependencies

--- setup.py
+++ setup.py
@@ -4,24 +4,29 @@ setup(
     name="training_center",
     version="0.1",
     description="Trains algorithms from NHANES dataset.",
-    packages=["fold_maker", "shape_age_range", "fit_running", "prediction", "feature_importances", "utils", "test"],
+    packages=[
+        "fold_maker",
+        "shape_age_range",
+        "fit_running",
+        "prediction",
+        "feature_importances",
+        "utils",
+        "test",
+    ],
     requires=["setuptools", "wheel"],
     install_requires=[
-        "numpy",
-        "pandas",
-        "pyarrow",
-        "hyperopt",
+        "numpy==1.16.6",
+        "pandas==1.2.4",
+        "pyarrow==4.0.1",
+        "hyperopt==0.2.5",
         "scikit-learn==0.23.2",
-        "scikit-survival",
-        "lightgbm",
-        "hyperopt",
-        "gspread",
-        "matplotlib", 
-        "openpyxl"
+        "scikit-survival==0.14.0",
+        "lightgbm==3.2.1",
+        "gspread==3.7.0",
+        "matplotlib==3.4.2",
+        "openpyxl==3.0.7",
     ],
-    extras_require={
-        "dev": ["tqdm", "jupyter", "ipympl", "black"]
-    },
+    extras_require={"dev": ["tqdm==4.61.1", "jupyter==1.0.0", "ipympl==0.7.0", "black==21.6b0"]},
     entry_points={
         "console_scripts": [
             "make_folds=fold_maker.fold_maker:fold_maker_cli",
