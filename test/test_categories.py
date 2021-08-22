4ad6e2e3535852c93b4017fcae297c0fc885af8a
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-08-22 11:38:29 -0400

    Passing tests

--- test/test_categories.py
+++ test/test_categories.py
@@ -9,14 +9,15 @@ class TestCategories(unittest.TestCase):
     def test_matching_categories(self):
         # Test if all categories are in the google sheet.
         for main_category in ["examination", "laboratory", "questionnaire"]:
-            categories_in_sheet = pd.Index(
-                get_col_values(main_category, "category")[3:]
-            )
-            categories_in_data = (
-                pd.Index(os.listdir(f"data/{main_category}"))
-                .str.split(".")
-                .map(lambda splitted_file: splitted_file[0])
-            )
+            for random_state in [1, 2]:
+                categories_in_sheet = pd.Index(
+                    get_col_values(f"{main_category} {random_state}", "category")[3:]
+                )
+                categories_in_data = (
+                    pd.Index(os.listdir(f"data/{main_category}"))
+                    .str.split(".")
+                    .map(lambda splitted_file: splitted_file[0])
+                )
 
-            self.assertTrue(all(categories_in_sheet.isin(categories_in_data)))
-            self.assertTrue(all(categories_in_data.isin(categories_in_sheet)))
+                self.assertTrue(all(categories_in_sheet.isin(categories_in_data)))
+                self.assertTrue(all(categories_in_data.isin(categories_in_sheet)))
